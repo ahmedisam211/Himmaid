3cea4d1295398c696bdf89ee4f76f96ce395aad6
ahmedisam211 <158821953+ahmedisam211@users.noreply.github.com>
2025-04-10 18:42:12 +0400

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,12 @@
+<header>
+  <nav class="navbar">
+    <h1>مكتبة حميد</h1>
+    <ul>
+      <li><a href="index.html">الرئيسية</a></li>
+      <li><a href="poems.php">القصائد</a></li>
+      <li><a href="media.html">الصوتيات والمرئيات</a></li>
+      <li><a href="about.html">عن حميد</a></li>
+      <li><a href="submit.php">شارك بمحتوى</a></li>
+    </ul>
+  </nav>
+</header>
